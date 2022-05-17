5612feb9187677a47072cf4b3b5513b035835959
HederAlves <alvesheder92@gmail.com>
2022-05-17 14:26:39 -0300

    Create

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <Login />
+</div>
+</template>
+<script>
+import Login from './components/Template/Login.vue';
+import Store from './components/Template/Store.vue';
+export default {
+    name: "App",
+    components: { Login, Store }
+}
+</script>
