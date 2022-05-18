1215803811c67770f40867ab57f6c1428eb2851b
HederAlves <alvesheder92@gmail.com>
2022-05-18 03:58:18 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
 <div>
-  <Login />
+  <router-view></router-view>
 </div>
 </template>
 <script>
-import Login from './components/Template/Login.vue';
-import Store from './components/Template/Store.vue';
+
 export default {
     name: "App",
-    components: { Login, Store }
 }
 </script>
