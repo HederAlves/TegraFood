3e67215841c33b419ea53bf15660635f1116b9c4
HederAlves <alvesheder92@gmail.com>
2022-06-01 15:45:57 -0300

    Create

---
+++ src/components/atmos/TheCardDiscountCoupon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="discountCouponCard">
+    <h1>Cupom de desconto</h1>
+    <input type="text" />
+    <button>Adicionar</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "TheCardDiscountCoupon",
+};
+</script>
+<style scoped>
+.discountCouponCard {
+  @apply flex justify-center mx-5 border;
+  width: 734px;
+  height: 128px;
+  border-radius: 8px;
+  box-shadow: 3px 3px 8px 1px rgba(0, 0, 0, 0.3);
+}
+
+h1 {
+  @apply text-3xl font-bold self-center mb-2 mr-3;
+  color: #223263;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+input {
+  align-self: center;
+  width: 260px;
+  height: 48px;
+  box-sizing: border-box;
+  border: solid 2px #ebf0ff;
+  border-radius: 5px;
+}
+
+input:focus {
+  outline: none;
+}
+
+Button {
+  align-self: center;
+  background: #dc9000;
+  color: white;
+  width: 140px;
+  height: 50px;
+  border-radius: 5px;
+  text-align: center;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/atmos/TheTablePay.vue
@@ -0,0 +1,48 @@
+<template>
+    <table>
+      <tr>
+        <td class="tableLeft">Subtotal</td>
+        <td class="tableRigth">R$ 83,00</td>
+      </tr>
+      <tr>
+        <td class="tableLeft">Entrega</td>
+        <td class="tableRigth"><a href="">Calcular</a></td>
+      </tr>
+      <tr>
+        <td class="tableLeft">Total</td>
+        <td class="tableRigth">R$ 83,00</td>
+      </tr>
+    </table>
+</template>
+
+<script>
+export default {
+  name: "TheTablePay",
+};
+</script>
+
+<style scoped>
+table {
+  border-collapse: separate;
+  border-spacing: 0;
+  height: 162px;
+}
+.tableRigth {
+  @apply text-3xl font-bold pr-5 text-right;
+  color: #223263;
+  border: 0.25px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0px 6px 6px 0px;
+  width: 200px;
+}
+.tableLeft {
+  @apply text-base font-bold pl-5 uppercase;
+  border: 0.25px solid rgba(0, 0, 0, 0.2);
+  border-radius: 6px 0px 0px 6px;
+  width: 250px;
+}
+a {
+  @apply text-sm text-black align-middle;
+  text-decoration-line: underline;
+  color: #223263;
+}
+</style>
