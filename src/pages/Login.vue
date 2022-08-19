1ceb60675ff8b0168496f8eded925d76a8d239b3
HederAlves <alvesheder92@gmail.com>
2022-08-18 23:24:31 -0300

    Update
    component Login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,53 +1,115 @@
 <template>
-      <TheFormLogin />
+  <div class="mainContainer">
+    <img
+      class="imageLogin"
+      src="/images/image-login.png"
+      alt="imagem de lanches"
+    />
+    <section class="sectionForm">
+      <h1 class="titleLogin">Bem vindo !</h1>
+      <p>Faça seu login para continuar</p>
+      <form class="loginForm" @submit.prevent="save">
+        <i class="inputContainer">
+          <img src="/icons/email.png" alt="" />
+          <input
+            class="inputLogin"
+            type="text"
+            placeholder="Seu e-mail"
+            required="Digite seu Email"
+          />
+        </i>
+        <i class="inputContainer">
+          <img class="" src="/icons/password.png" alt="" />
+          <input
+            class="inputLogin"
+            type="text"
+            placeholder="Senha"
+            required="Digite sua Senha"
+          />
+        </i>
+        <button class="buttonPrimary">Entrar</button>
+      </form>
+      <span>
+        <p class="lineDivision">ou</p>
+      </span>
+      <button class="buttonGoogle">Entrar com o Google</button>
+      <p>Não tem uma conta?<a href="">Cadastre</a></p>
+    </section>
+  </div>
 </template>
-  
-  <script>
-import TheFormLogin from '../components/templates/TheFormLogin.vue';
+
+<script>
 export default {
-    name: "Login",
-    components: { TheFormLogin }
+  name: "app",
 };
 </script>
-  
+
 <style scoped>
 .mainContainer {
-  @apply flex justify-between;
-  color: #9098B1;
+  @apply flex;
+  color: #9098b1;
+}
+.imageLogin {
+  @apply h-screen;
 }
 .sectionForm {
-  @apply flex flex-col justify-around items-center mt-8 mr-72;
+  @apply pt-20 pl-72 flex flex-col justify-around items-center;
   height: 560px;
+}
+.loginForm {
+  @apply flex flex-col justify-around h-52;
 }
 .titleLogin {
   @apply font-bold text-2xl text-black;
 }
-.buttonGoogle {
-  color: #9098B1;
-  width: 343px;
-  height: 57px;
-  border: solid 2px #EBF0FF;
+.inputContainer {
+  @apply flex p-2;
+  width: 343;
+  height: 48px;
+  box-sizing: border-box;
+  border: solid 2px #ebf0ff;
   border-radius: 5px;
 }
-.iconGoogle{
+.inputLogin {
   position: absolute;
-  margin-left: 8px;
+  margin: -8px 0px 0px 36px;
+  width: 343;
+  height: 44px;
+}
+.inputLogin:focus {
+  box-shadow: 0 0 0 0;
+  border: 0 none;
+  outline: 0;
+}
+.buttonPrimary {
+  background: #dc9000;
+  color: white;
+  width: 343px;
+  height: 57px;
+  border-radius: 5px;
+}
+.buttonGoogle {
+  color: #9098b1;
+  width: 343px;
+  height: 57px;
+  border: solid 2px #ebf0ff;
+  border-radius: 5px;
 }
 span {
   box-sizing: border-box;
-  border-top: 2px solid #EBF0FF;
+  border-top: 2px solid #ebf0ff;
   width: 346px;
   text-align: center;
 }
 .lineDivision {
   position: absolute;
-  margin: -16px 0px 0px 143px;
+  margin: -16px 0px 0px 145px;
   background: white;
   padding: 0px 22px 0px 22px;
 }
-.stringRouter{
-    color:darkturquoise;
-    font-weight: bold;
-    padding-left: 5px;
+a {
+  color: blue;
+  font-weight: bold;
+  margin-left: 8px;
 }
 </style>
